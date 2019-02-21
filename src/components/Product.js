014e7a102e4f3af77cb8f7ba1253ba18d441355b
Ali <ar.k@outlook.com>
2019-02-21 07:39:52 -0800

    implement context to pass data between components

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,12 +3,16 @@ import styled from "styled-components";
 
 class Product extends Component {
   render() {
-    const { title } = this.props.product;
+    const { title, price, img } = this.props.product;
     return (
-      <ProductWrapper className="col-9 col-md-6 col-lg-3 my-3 mx-auto">
+      <ProductWrapper className="col-9 col-md-6 col-lg-6 my-3 mx-auto">
         <div className="card p-5 mt-5">
-          <div className="card-body">
-            <h2 className="card-title">{title}</h2>
+          <div className="img-container p-5">
+            <img src={img} className="card-img-top" alt={title} />
+            <div className="card-body">
+              <h2 className="card-title">{title}</h2>
+              <p>Price: ${price}</p>
+            </div>
           </div>
         </div>
       </ProductWrapper>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import Title from "./Title";
 import Product from "./Product";
+import { ProductConsumer } from "../context";
 
 class ProductList extends Component {
   render() {
-    console.log(this.props.products);
     return (
-      <div className="row">
-        <div className="col-md">
-          <Title name="our" title="products" />
-        </div>
-        <div className="row">
-          {this.props.products.map(product => (
-            <Product key={product.id} product={product} />
-          ))}
-        </div>
-      </div>
+      <ProductConsumer>
+        {data => (
+          <div className="row">
+            <div className="col-md">
+              <Title name="our" title="products" />
+            </div>
+            <div className="row">
+              {data.map(product => (
+                <Product key={product.id} product={product} />
+              ))}
+            </div>
+          </div>
+        )}
+      </ProductConsumer>
     );
   }
 }
